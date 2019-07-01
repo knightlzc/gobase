9f0cc746f2011cf6ebca9baffb07636afd3a9105
zicui.liu <zicui.liu@renren-inc.com>
2019-07-01 16:17:51 +0800

    controller

---
+++ gobase-wx-console/src/main/java/com/gobase/web/controller/order/OrderController.java
@@ -0,0 +1,23 @@
+/**
+ * <p>Copyright: All Rights Reserved</p>  
+ * <p>Description:  </p>
+ * <p>Author:zcliu/刘子萃</p>
+ * @Title: OrderController.java
+ * @Package com.gobase.web.controller.order
+ * @date 2019年7月1日 下午4:17:04 
+ */
+package com.gobase.web.controller.order;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** 
+ * <p>Copyright: All Rights Reserved</p>  
+ * <p>Description:  </p> 
+ * <p>Author:zcliu/刘子萃</p>
+ */
+@RequestMapping("order")
+@RestController
+public class OrderController {
+
+}
---
+++ gobase-wx-console/src/main/java/com/gobase/web/controller/shop/ShopController.java
@@ -0,0 +1,23 @@
+/**
+ * <p>Copyright: All Rights Reserved</p>  
+ * <p>Description:  </p>
+ * <p>Author:zcliu/刘子萃</p>
+ * @Title: ShopController.java
+ * @Package com.gobase.web.controller.shop
+ * @date 2019年7月1日 下午4:17:35 
+ */
+package com.gobase.web.controller.shop;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** 
+ * <p>Copyright: All Rights Reserved</p>  
+ * <p>Description:  </p> 
+ * <p>Author:zcliu/刘子萃</p>
+ */
+@RequestMapping("shop")
+@RestController
+public class ShopController {
+
+}
