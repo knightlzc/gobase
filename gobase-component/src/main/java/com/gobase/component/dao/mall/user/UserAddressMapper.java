a101a430a87b875f0195bc1c88aa0c65edbcbe34
mingmingsun <mingming.sun@renren-inc.com>
2019-07-18 11:17:02 +0800

    userAddress

---
+++ gobase-component/src/main/java/com/gobase/component/dao/mall/user/UserAddressMapper.java
@@ -0,0 +1,9 @@
+package com.gobase.component.dao.mall.user;
+
+import com.gobase.component.bean.mall.user.UserAddress;
+import com.gobase.component.bean.mall.user.UserAddressExample;
+
+import com.gobase.component.dao.IBaseMapper;
+
+public interface UserAddressMapper extends IBaseMapper<UserAddressExample, UserAddress> {
+}
