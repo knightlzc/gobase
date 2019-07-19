c5fe4b27f5d4155ab5993fa413441e69e2e3355e
mingmingsun <mingming.sun@renren-inc.com>
2019-07-19 16:33:37 +0800

    user

--- gobase-component/src/main/java/com/gobase/component/dao/mall/user/UserMapper.java
+++ gobase-component/src/main/java/com/gobase/component/dao/mall/user/UserMapper.java
@@ -2,8 +2,8 @@ package com.gobase.component.dao.mall.user;
 
 import com.gobase.component.bean.mall.user.User;
 import com.gobase.component.bean.mall.user.UserExample;
+
 import com.gobase.component.dao.IBaseMapper;
 
-public interface UserMapper extends IBaseMapper<UserExample, User>{
-    
+public interface UserMapper extends IBaseMapper<UserExample, User> {
 }
