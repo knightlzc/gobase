7e8f52a4702222f5ea147262cafce2c41f278ee6
zicui.liu <zicui.liu@renren-inc.com>
2019-07-20 16:05:24 +0800

    上传照片接口

---
+++ gobase-wx-console/src/main/java/com/gobase/web/controller/upload/response/ImgResponse.java
@@ -0,0 +1,34 @@
+/**
+ * <p>Copyright: All Rights Reserved</p>  
+ * <p>Description:  </p>
+ * <p>Author:zcliu/刘子萃</p>
+ * @Title: ImgResponse.java
+ * @Package com.gobase.web.controller.upload.response
+ * @date 2019年7月20日 下午3:05:01 
+ */
+package com.gobase.web.controller.upload.response;
+
+/** 
+ * <p>Copyright: All Rights Reserved</p>  
+ * <p>Description:  </p> 
+ * <p>Author:zcliu/刘子萃</p>
+ */
+
+public class ImgResponse {
+	
+	public ImgResponse() {}
+	
+	public ImgResponse(String url) {
+		this.url = url;
+	}
+
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
