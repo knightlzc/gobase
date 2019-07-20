a3b91af3a74c8b898b847cf4905bc808e9ae9e3d
zicui.liu <zicui.liu@renren-inc.com>
2019-07-20 18:52:10 +0800

    init

--- gobase-wx-console/src/test/java/test/gobase/InitSql.java
+++ gobase-wx-console/src/test/java/test/gobase/InitSql.java
@@ -43,19 +43,19 @@ public class InitSql {
 		for (int i = 0; i < 15; i++) {
 			String goodsId = IDCreater.generate("GO");
 			String name = i*10+"元优惠券";
-			String img = "https://wx.apricot32.cn/img/2019/07/20/img08000011907201832.jpg";
+			String img = "https://wx.apricot32.cn/img/2019/07/20/img08000001907201851.jpg";
 			int shopId = 2;
 			int price = i*10;
 			String c1="shkj";
-			String c2="rcyp";
+			String c2="tq";
 			String c3="ys";
 			printGoodsSql(goodsId, name, img, shopId, price, c1, c2, c3);
 		}
 	}
 	
 	public static void printGoodsSql(String goodsId,String name,String img,int shopId,int price,String c1,String c2,String c3) {
-		System.out.println("INSERT INTO `goods` (goods_id,tiny_name,full_name,big_img,small_img,category1,category2,category3,shop_id,real_price) "
-				+ "VALUES ('"+goodsId+"', '"+name+"', '"+name+"', '"+img+"', '"+img+"', '"+c1+"', '"+c2+"', '"+c3+"',"+shopId+","+price+" );");
+		System.out.println("INSERT INTO `goods` (goods_id,tiny_name,full_name,big_img,small_img,category1,category2,category3,shop_id,real_price,description) "
+				+ "VALUES ('"+goodsId+"', '"+name+"', '"+name+"', '"+img+"', '"+img+"', '"+c1+"', '"+c2+"', '"+c3+"',"+shopId+","+price+",'"+name+"' );");
 		
 	}
 
